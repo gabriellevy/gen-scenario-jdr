705a6c57a86c022a24c31299588fbc24bb4778ea
gabriellevy <mdeharbe@hotmail.com>
2024-09-22 18:37:01 +0200

    ajout de mui avec exemples de test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { useForm, SubmitHandler } from "react-hook-form"
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import HomeIcon from '@mui/icons-material/Home';
+import { Button } from '@mui/material';
 
 type Inputs = {
   example: string,
@@ -18,17 +21,23 @@ export default function App() {
   console.log(watch("example")) // watch input value by passing the name of it
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+    <div>
+      <HomeIcon color="secondary"/>
+      <AccessAlarmIcon color="secondary"/>
+      {/** "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
 
-      <input type="submit" />
-    </form>
+        <Button variant="contained">
+          <input type="submit" />
+        </Button>
+      </form>
+    </div>
   )
 }
